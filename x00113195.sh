de26de1b915633295425244ed56055b36203bdf6
mint <x00113195@gmail.com>
2014-10-16 10:54:16 +0100

    My grades test script

---
+++ x00113195.sh
@@ -0,0 +1,43 @@
+
+
+#Test 1
+#Inputs : 40 40
+#Expected : Fail
+#Actaual Output : Fail
+echo Test 1 Grades 40 40
+java grades 40 40
+
+#Test 2
+#Inputs : 20 60
+#Expected : Component Fail
+#Actaual Output : Component Fail
+echo Test 2 Grades 20 60
+java grades 20 60
+
+#Test 3
+#Inputs : 100 30
+#Expected : Component Fail
+#Actaual Output : Component Fail
+echo Test 3 Grades 100 30
+java grades 100 30
+
+#Test 4
+#Inputs : 50 50
+#Expected : Pass
+#Actaual Output : Fail
+echo Test 4 Grades 50 50
+java grades 50 50
+
+#Test 5
+#Inputs : 80 90
+#Expected : Pass with distinction
+#Actaual Output : Pass with distinction
+echo Test 5 Grades 80 90
+java grades 80 90
+
+#Test 6
+#Inputs : 101 102
+#Expected : Pass with distinction
+#Actaual Output : Pass with distinction
+echo Test 6 Grades 101 102
+java grades 101 102
