e716b443f616e6fd24cc78e5bca7e7abcc38ba52
x00099862 <x00099862@gmail.com>
2014-10-16 11:09:01 +0100

    My grades

---
+++ x00099862.sh
@@ -0,0 +1,35 @@
+#Test Case 1
+#Inputs: 40 40
+#Expected output: Fail
+#Actual Output: Fail
+java grades 40 40
+
+#Test Case 2
+#Inputs: 25 35
+#Expected output: Component Fail
+#Actual output: Component Fail
+java grades 25 35
+
+#Test Case 3
+#Inputs: 20 30
+#Expected output: Component Fail
+#Actual outputs: Component Fail
+java grades 20 30
+
+#Test Case 4
+#Inputs: 50 50
+#Expected output: Pass
+#Actual output: Fail
+java grades 50 50
+
+#Test Case 5
+#Inputs: 60 70
+#Expected output: Pass with Distinction
+#Actual output: Pass with Distinction
+java grades 85 100
+
+#Test Case 6
+#Inputs: 101 110
+#Expected output: Invalid Input
+#Actual output: Invalid Input
+java grades 101 110
